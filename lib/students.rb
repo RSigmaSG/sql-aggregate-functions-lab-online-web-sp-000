fa7c3133530665a83e94d2b19894a33ebc2d0990
Sumer   Gaikwad <sg.comparch@gmail.com>
2021-01-04 21:14:47 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -17,5 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  "SELECT SUM(tardies) FROM students where grade ;"
+  "SELECT SUM(tardies) FROM students where grade = 9;"
 end
